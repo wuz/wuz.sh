0f7b6bb398610356fa583c0d58095d0baa0d8a6c
wuz <conlin.durbin@whatnot.com>
2024-10-08 11:38:59 -0400

    Add plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { themeClass } from "./theme.css";
 import "./global.css";
 import Favicon from "@/components/favicon";
 
-
 export const metadata: Metadata = {
   title: "Conlin Durbin",
   description: "Senior Frontend Engineer. Typescripter. TTRPG-er",
@@ -19,6 +18,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Favicon />
+        <script
+          defer
+          data-domain="wuz.sh"
+          src="https://plausible.io/js/script.js"
+        ></script>
       </head>
       <body className={themeClass}>
         <div className="content">{children}</div>
