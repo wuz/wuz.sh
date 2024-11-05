a30ad56241d0399a1e7a3de1a64a9d26aee76284
wuz <conlin.durbin@whatnot.com>
2024-11-05 10:57:32 -0500

    Fix posts

--- content-collections.ts
+++ content-collections.ts
@@ -4,47 +4,50 @@ import { transformerTwoslash } from "@shikijs/twoslash";
 import { defineCollection, defineConfig } from "@content-collections/core";
 import { compileMDX } from "@content-collections/mdx";
 import {
-	transformerNotationDiff,
-	transformerNotationHighlight,
-	transformerNotationWordHighlight,
-	transformerMetaHighlight,
-	transformerMetaWordHighlight,
+  transformerNotationDiff,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+  transformerMetaHighlight,
+  transformerMetaWordHighlight,
 } from "@shikijs/transformers";
 
 const shikiOptions = {
-	theme: "min-dark",
-	transformers: [
-		transformerTwoslash(),
-		transformerNotationDiff(),
-		transformerNotationHighlight(),
-		transformerNotationWordHighlight(),
-		transformerMetaHighlight(),
-		transformerMetaWordHighlight(),
-	],
+  theme: "min-dark",
+  transformers: [
+    transformerTwoslash({
+      explicitTrigger: true,
+    }),
+    transformerNotationDiff(),
+    transformerNotationHighlight(),
+    transformerNotationWordHighlight(),
+    transformerMetaHighlight(),
+    transformerMetaWordHighlight(),
+  ],
 };
 
 const posts = defineCollection({
-	name: "posts",
-	directory: "posts",
-	include: "**/*.mdx",
-	schema: (z) => ({
-		title: z.string(),
-		description: z.string().optional(),
-		published: z.boolean(),
-		date: z.string().optional(),
-		coverImage: z.string().optional(),
-	}),
-	transform: async (document, context) => {
-		const mdx = await compileMDX(context, document, {
-			rehypePlugins: [[rehypeShiki, shikiOptions]],
-		});
-		return {
-			...document,
-			mdx,
-		};
-	},
+  name: "posts",
+  directory: "posts",
+  include: "**/*.mdx",
+  schema: (z) => ({
+    title: z.string(),
+    description: z.string().optional(),
+    published: z.boolean(),
+    date: z.string().optional(),
+    coverImage: z.string().optional(),
+  }),
+  transform: async (document, context) => {
+    const mdx = await compileMDX(context, document, {
+      rehypePlugins: [[rehypeShiki, shikiOptions]],
+    });
+    return {
+      ...document,
+      url: `/posts/${document._meta.path}`,
+      mdx,
+    };
+  },
 });
 
 export default defineConfig({
-	collections: [posts],
+  collections: [posts],
 });
--- src/components/posts-display/index.tsx
+++ src/components/posts-display/index.tsx
@@ -4,51 +4,51 @@ import { listItem, listPost } from "./posts-display.css";
 import { Heading } from "../type";
 
 export async function generateStaticParams() {
-	return allPosts
-		.filter((post) => post.published)
-		.map((post) => ({
-			slug: post._meta.path,
-		}));
+  return allPosts
+    .filter((post) => post.published)
+    .map((post) => ({
+      slug: post._meta.path,
+    }));
 }
 
 const SinglePost = ({ post }: { post: Post }) => {
-	const date = post.date ? new Date(post.date) : null;
-	return (
-		<li className={listItem}>
-			{date && (
-				<time
-					dateTime={date.toISOString()}
-					className="mb-2 block text-xs text-gray-600"
-				>
-					{format(date, "MMM d, yyyy")}
-				</time>
-			)}
-			<a href={post._meta.path}>{post.title}</a>
-		</li>
-	);
+  const date = post.date ? new Date(post.date) : null;
+  return (
+    <li className={listItem}>
+      {date && (
+        <time
+          dateTime={date.toISOString()}
+          className="mb-2 block text-xs text-gray-600"
+        >
+          {format(date, "MMM d, yyyy")}
+        </time>
+      )}
+      <a href={post.url}>{post.title}</a>
+    </li>
+  );
 };
 
 type PostsDisplayProps = {
-	limit?: number;
+  limit?: number;
 };
 
 export default async function PostsDisplay({ limit = 5 }: PostsDisplayProps) {
-	let posts = allPosts
-		.filter((post) => post.published && post.date)
-		.sort((a, b) =>
-			a.date && b.date ? compareDesc(new Date(a.date), new Date(b.date)) : 0,
-		);
-	if (limit) {
-		posts = posts.slice(0, limit);
-	}
-	return (
-		<div className="flow">
-			<Heading level="3">Recent Writing</Heading>
-			<ul role="list" className={listPost}>
-				{posts.map((post) => (
-					<SinglePost post={post} key={post._meta.path} />
-				))}
-			</ul>
-		</div>
-	);
+  let posts = allPosts
+    .filter((post) => post.published && post.date)
+    .sort((a, b) =>
+      a.date && b.date ? compareDesc(new Date(a.date), new Date(b.date)) : 0,
+    );
+  if (limit) {
+    posts = posts.slice(0, limit);
+  }
+  return (
+    <div className="flow">
+      <Heading level="3">Recent Writing</Heading>
+      <ul role="list" className={listPost}>
+        {posts.map((post) => (
+          <SinglePost post={post} key={post._meta.path} />
+        ))}
+      </ul>
+    </div>
+  );
 }
