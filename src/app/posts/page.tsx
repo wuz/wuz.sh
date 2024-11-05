5253cc84c76bfdc3f577ad63c130f883cced7425
wuz <conlin.durbin@whatnot.com>
2024-11-05 09:44:35 -0500

    Fix content collections

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,26 +2,26 @@
 import { GridLayout, GridLeft, GridRight } from "@/components/grid-layout";
 import Header from "@/components/header";
 import PostsDisplay from "@/components/posts-display";
-import { allPosts } from "contentlayer/generated";
+import { allPosts } from "content-collections";
 
 export async function generateStaticParams() {
-	return allPosts
-		.filter((post) => post.published)
-		.map((post) => ({
-			slug: post._raw.flattenedPath,
-		}));
+  return allPosts
+    .filter((post) => post.published)
+    .map((post) => ({
+      slug: post._meta.path,
+    }));
 }
 
 export default async function Page() {
-	return (
-		<>
-			<Header />
-			<GridLayout>
-				<GridLeft>A collection of things I&apos;ve written.</GridLeft>
-				<GridRight>
-					<PostsDisplay limit={undefined} />
-				</GridRight>
-			</GridLayout>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <GridLayout>
+        <GridLeft>A collection of things I&apos;ve written.</GridLeft>
+        <GridRight>
+          <PostsDisplay limit={undefined} />
+        </GridRight>
+      </GridLayout>
+    </>
+  );
 }
--- src/components/posts-display/index.tsx
+++ src/components/posts-display/index.tsx
@@ -1,4 +1,4 @@
-import { type Post, allPosts } from "contentlayer/generated";
+import { type Post, allPosts } from "content-collections";
 import { compareDesc, format, parseISO } from "date-fns";
 import { listItem, listPost } from "./posts-display.css";
 import { Heading } from "../type";
@@ -7,18 +7,23 @@ export async function generateStaticParams() {
 	return allPosts
 		.filter((post) => post.published)
 		.map((post) => ({
-			slug: post._raw.flattenedPath,
+			slug: post._meta.path,
 		}));
 }
 
 const SinglePost = ({ post }: { post: Post }) => {
-	// const MDXContent = useMDXComponent(post.body.code);
+	const date = post.date ? new Date(post.date) : null;
 	return (
 		<li className={listItem}>
-			<time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-				{format(parseISO(post.date), "MMM d, yyyy")}
-			</time>
-			<a href={post.url}>{post.title}</a>
+			{date && (
+				<time
+					dateTime={date.toISOString()}
+					className="mb-2 block text-xs text-gray-600"
+				>
+					{format(date, "MMM d, yyyy")}
+				</time>
+			)}
+			<a href={post._meta.path}>{post.title}</a>
 		</li>
 	);
 };
@@ -29,8 +34,10 @@ type PostsDisplayProps = {
 
 export default async function PostsDisplay({ limit = 5 }: PostsDisplayProps) {
 	let posts = allPosts
-		.filter((post) => post.published)
-		.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+		.filter((post) => post.published && post.date)
+		.sort((a, b) =>
+			a.date && b.date ? compareDesc(new Date(a.date), new Date(b.date)) : 0,
+		);
 	if (limit) {
 		posts = posts.slice(0, limit);
 	}
@@ -39,7 +46,7 @@ export default async function PostsDisplay({ limit = 5 }: PostsDisplayProps) {
 			<Heading level="3">Recent Writing</Heading>
 			<ul role="list" className={listPost}>
 				{posts.map((post) => (
-					<SinglePost post={post} key={post._id} />
+					<SinglePost post={post} key={post._meta.path} />
 				))}
 			</ul>
 		</div>
